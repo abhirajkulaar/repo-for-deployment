cbdda5e6ad1cf372e5733c3d7c681d7ab32405f2
unknown <abhirajkulaar@gmail.com>
2022-08-13 14:15:55 +0530

    get mongo URL from env

--- services/database.js
+++ services/database.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGO_URL;
 const client = new MongoClient(url);
 const dbName = 'products';
 
